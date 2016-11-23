3df6ffb37f1c919ef025a1519b96bc4deedc5051
MaxMyLastName <mopushnyev@gmail.com>
2016-11-22 22:02:51 -0800

    Add Window size check

--- myTinyGPS.cpp
+++ myTinyGPS.cpp
@@ -3,24 +3,37 @@ using namespace std;
 
 int main() {
 
+	int maxY, maxX;
 
 	USART_GPS myGPS;
 
-
    initscr(); // initialize Ncurses
+   getmaxyx( stdscr, maxY, maxX );
    noecho();
    nodelay(stdscr, TRUE);
    curs_set(0);
 
+   //printw("X: %i, Y: %i", maxX, maxY);
+
    while(getch() != 'q'){
 
-	   	   	myGPS.GPS_get();
-   			myGPS.GPSdata_showSpeed(myGPS.get_GndSpdKts(), Kts);
-   			myGPS.GPSdata_w_print();
-   			refresh();
-   			wmove(stdscr, 0, 0);
+	   // Window Size must be at least X=53, Y=20 for the numbers to fit
+	   if(maxX >= 53 && maxY >= 20){
 
-   	    }//end while
+	   	   	if(myGPS.GPS_get() == 1){
+	   	   		myGPS.GPSdata_showSpeed(myGPS.get_GndSpdKts(), Kts);
+	   	   		myGPS.GPSdata_w_print();
+	   	   		refresh();
+	   	   		wmove(stdscr, 0, 0);
+	   	   	}
+
+	   }else{
+		   printw("Window size too small...");
+		   refresh();
+	   	   wmove(stdscr, 0, 0);
+	   }
+   }//end while
+
 
 
    endwin();
